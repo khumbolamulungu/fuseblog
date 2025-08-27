cfcf93897138ef4fd8f4b61387c6821e4690500e
Khumbo Klein Chilamwa <68913833+khumbolamulungu@users.noreply.github.com>
2025-08-27 15:51:18 +0200

    second commit

--- src/app/dashboard/edit/[id]/page.tsx
+++ src/app/dashboard/edit/[id]/page.tsx
@@ -4,11 +4,13 @@ import { prisma } from "@/app/utils/db";
 import { notFound } from "next/navigation";
 
 export default async function EditPostPage({ params }: { params: { id: string } }) {
-    const post = await prisma.blogPost.findUnique({
-        where: { id: params.id },
-    });
+  const post = await prisma.blogPost.findUnique({
+    where: { id: Number(params.id) }, // 👈 convert string to number
+  });
 
-    if (!post) return notFound();
+  if (!post) return notFound();
 
-    return <UpdatePost post={post} />;
+  return <UpdatePost post={post} />;
 }
+
+
